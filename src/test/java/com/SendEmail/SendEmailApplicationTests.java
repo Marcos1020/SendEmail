152c2ad07f2a7564a9fbee4aa129dffa9b5b8212
marcosvinicius <marcossanches710@gmail.com>
2023-04-07 15:04:22 -0300

    APlicação de envio de email

--- src/test/java/com/SendEmail/SendEmailApplicationTests.java
+++ src/test/java/com/SendEmail/SendEmailApplicationTests.java
@@ -1,8 +1,8 @@
-package com.Clientes;
+package com.SendEmail;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class 
-ClientesApplicationTests {
+class
+SendEmailApplicationTests {
 
 }
